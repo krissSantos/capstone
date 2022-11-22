fddac77aa685eb6d16a7f7f78dc12f50250de90a
IamBadAlpha <romeonombrejr@gmail.com>
2022-11-22 11:41:24 +0800

    addition of modal on the cart.blade when user is not logged in

--- Bakeshop/resources/views/cart.blade.php
+++ Bakeshop/resources/views/cart.blade.php
@@ -95,9 +95,34 @@
                 hidden
                 />
             @endfor
-            <button type="submit">Place order</button>
+
+            @if (Session::get('id'))
+                <button type="submit" class="btn btn-success">Place Order</button>
+            @else
+            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Place Order
+            </button>
+            @endif
+            
         </ul>
     </form>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Error</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                You must log in first before you can proceed!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" onclick="window.open('/login/user');" >Go to Log In</button>
+            </div>
+            </div>
+        </div>
+    </div>
 @include('layouts/footer')
 </body>
 </html>
